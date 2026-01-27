a96e94616ab480180c3ac43536663c9e950985f2
Igorek04 <burlik.igor2016@gmail.com>
2026-01-27 15:58:11 +0000

    Task 22 part 1

---
+++ app/code/Perspective/CatalogCategoryAnalytics/ViewModel/CategoryAnalytics.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Perspective\CatalogCategoryAnalytics\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class CategoryAnalytics implements ArgumentInterface
+{
+
+}
